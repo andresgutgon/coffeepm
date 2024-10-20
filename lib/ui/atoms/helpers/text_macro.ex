3296df8a4f7c0aa9c51707267bc25be6e658acd7
andresgutgon <andresgutgon@gmail.com>
2024-10-20 17:46:19 +0200

    feat: wip

--- lib/ui/atoms/helpers/text_macro.ex
+++ lib/ui/atoms/helpers/text_macro.ex
@@ -5,44 +5,172 @@ defmodule UI.Atoms.Helpers.TextMacro do
   import UI.Tokens.Color, only: [color: 2]
 
   defmacro generate_sizes_macro do
-    sizes = ~w(h1 h1b h2 h2b h3 h4 h5 h6 h7)a
+    sizes = [
+      %{name: :h1, attributes: [{:size, :string, "h1"}]},
+      %{
+        name: :h1b,
+        attributes: [{:size, :string, "h1"}, {:weight, :string, "bold"}]
+      },
+      %{name: :h2, attributes: [{:size, :string, "h2"}]},
+      %{
+        name: :h2b,
+        attributes: [{:size, :string, "h2"}, {:weight, :string, "bold"}]
+      },
+      %{name: :h3, attributes: [{:size, :string, "h3"}]},
+      %{
+        name: :h3b,
+        attributes: [{:size, :string, "h3"}, {:weight, :string, "bold"}]
+      },
+      %{name: :h4, attributes: [{:size, :string, "h4"}]},
+      %{
+        name: :h4m,
+        attributes: [{:size, :string, "h4"}, {:weight, :string, "medium"}]
+      },
+      %{
+        name: :h4b,
+        attributes: [{:size, :string, "h4"}, {:weight, :string, "semibold"}]
+      },
+      %{name: :h5, attributes: [{:size, :string, "h5"}]},
+      %{
+        name: :h5m,
+        attributes: [{:size, :string, "h5"}, {:weight, :string, "medium"}]
+      },
+      %{
+        name: :h5b,
+        attributes: [{:size, :string, "h5"}, {:weight, :string, "semibold"}]
+      },
+      %{name: :h6, attributes: [{:size, :string, "h6"}]},
+      %{
+        name: :h6m,
+        attributes: [{:size, :string, "h6"}, {:weight, :string, "medium"}]
+      },
+      %{
+        name: :h6b,
+        attributes: [{:size, :string, "h6"}, {:weight, :string, "semibold"}]
+      },
+      %{
+        name: :h6c,
+        attributes: [
+          {:size, :string, "h6"},
+          {:weight, :string, "bold"},
+          {:uppercase, :boolean, true}
+        ]
+      },
+      %{
+        name: :h7,
+        attributes: [
+          {:size, :string, "h7"},
+          {:weight, :string, "bold"},
+          {:spacing, :string, "wide"}
+        ]
+      },
+      %{
+        name: :h7c,
+        attributes: [
+          {:size, :string, "h7"},
+          {:weight, :string, "bold"},
+          {:uppercase, :boolean, true},
+          {:spacing, :string, "wide"}
+        ]
+      },
+      %{
+        name: :h8,
+        attributes: [
+          {:size, :string, "h8"},
+          {:weight, :string, "bold"},
+          {:spacing, :string, "wide"}
+        ]
+      }
+    ]
 
-    for size <- sizes do
-      quote do
-        @doc """
-        Component for #{unquote(size)} text.
+    common_attributes = [
+      {:tag, :string, "span"},
+      {:color, :string, "foreground"},
+      {:family, :string, "sans", ~w(sans mono)},
+      {:weight, :string, "normal"},
+      {:align, :string, "left"},
+      {:tracking, :string, "normal"},
+      {:display, :string, "inline"},
+      {:white_space, :string, "normal"},
+      {:word_break, :string, "normal"},
+      {:uppercase, :boolean, false},
+      {:capitalize, :boolean, false},
+      {:ellipsis, :boolean, false},
+      {:user_select, :boolean, true},
+      {:no_wrap, :boolean, false},
+      {:underline, :boolean, false},
+      {:line_through, :boolean, false},
+      {:monospace, :boolean, false},
+      {:centered, :boolean, false},
+      {:animate, :boolean, false},
+      {:class, :string, ""}
+    ]
+
+    for %{name: name, attributes: specific_attributes} <- sizes do
+      common_attributes_to_apply =
+        Enum.reject(common_attributes, fn common_attr ->
+          attr_name = elem(common_attr, 0)
+
+          Enum.any?(specific_attributes, fn
+            {specific_attr_name, _, _} -> specific_attr_name == attr_name
+            {specific_attr_name, _, _, _} -> specific_attr_name == attr_name
+          end)
+        end)
+
+      combined_attributes =
+        Enum.map(
+          specific_attributes ++ common_attributes_to_apply,
+          &Macro.escape/1
+        )
+
+      quote bind_quoted: [name: name, combined_attributes: combined_attributes] do
+        doc_string = """
+        Component for #{name} text.
         ## Examples
           alias UI.Text, as: Text
 
-          <Text.#{unquote(size)}>Text content</Text.#{unquote(size)}>
+          <Text.#{name}>Text content</Text.#{name}>
         """
 
-        attr :tag, :string, default: "span"
-        attr :family, :string, default: "sans", values: ~w(sans mono)
-        attr :align, :string, default: "left"
-        attr :color, :string, default: "foreground"
-        attr :tracking, :string, default: "normal"
-        attr :weight, :string, default: "normal"
-        attr :display, :string, default: "inline"
-        attr :white_space, :string, default: "normal"
-        attr :word_break, :string, default: "normal"
-        attr :uppercase, :boolean, default: false
-        attr :capitalize, :boolean, default: false
-        attr :ellipsis, :boolean, default: false
-        attr :user_select, :boolean, default: true
-        attr :no_wrap, :boolean, default: false
-        attr :underline, :boolean, default: false
-        attr :line_through, :boolean, default: false
-        attr :monospace, :boolean, default: false
-        attr :centered, :boolean, default: false
-        attr :animate, :boolean, default: false
-        attr :class, :string, default: ""
-        attr :rest, :global, default: %{}
+        @doc doc_string
 
+        combined_attributes =
+          Enum.map(combined_attributes, fn
+            {:{}, [], inner} ->
+              case inner do
+                [attr_name, type, default] ->
+                  [attr_name, type, default]
+
+                [attr_name, type, default, values] ->
+                  [attr_name, type, default, values]
+
+                _ ->
+                  inner
+              end
+
+            {attr_name, type, default} ->
+              [attr_name, type, default]
+
+            {attr_name, type, default, values} ->
+              [attr_name, type, default, values]
+          end)
+
+        for combined_attr <- combined_attributes do
+          case combined_attr do
+            [attr_name, type, default] when type != :global ->
+              attr attr_name, type, default: Macro.escape(default)
+
+            [attr_name, type, default, values] ->
+              attr attr_name, type,
+                default: Macro.escape(default),
+                values: Macro.escape(values)
+          end
+        end
+
+        attr :rest, :global, default: %{}
         slot :inner_block, required: true
 
-        def unquote(:"#{size}")(assigns) do
-          assigns = Map.put_new(assigns, :size, unquote(size))
+        def unquote(name)(assigns) do
           UI.Atoms.Helpers.TextMacro.text(assigns)
         end
       end
--- test/ui/atoms/text_test.exs
+++ test/ui/atoms/text_test.exs
@@ -7,51 +7,12 @@ defmodule UI.Atoms.TextTest do
   describe "Text component" do
     test "renders h1" do
       assigns = %{}
+
       assert(
         rendered_to_string(~H"""
         <Text.h1>The text</Text.h1>
         """) =~
           ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-h1 text-4xl user-select\">\n  The text\n</span>)
-      )
-    end
-
-    test "renders h1b" do
-      assigns = %{}
-      assert(
-        rendered_to_string(~H"""
-        <Text.h1b>The text</Text.h1b>
-        """) =~
-          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-h1 text-4xl user-select\">\n  The text\n</span>)
-      )
-    end
-
-    test "renders h2" do
-      assigns = %{}
-      assert(
-        rendered_to_string(~H"""
-        <Text.h2>The text</Text.h2>
-        """) =~
-          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-10 text-h2 user-select\">\n  The text\n</span>)
-      )
-    end
-
-    test "renders h2b" do
-      assigns = %{}
-      assert(
-        rendered_to_string(~H"""
-        <Text.h2b>The text</Text.h2b>
-        """) =~
-          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-10 text-h2 user-select\">\n  The text\n</span>)
-      )
-    end
-
-    test "renders h3" do
-      assigns = %{}
-      assert(
-        rendered_to_string(~H"""
-        <Text.h3>The text</Text.h3>
-        """) =~
-          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-10 text-h2 user-select\">\n  The text\n</span>)
       )
     end
   end
