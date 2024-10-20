73f808562c14f1cb792e93573a9081501b632492
andresgutgon <andresgutgon@gmail.com>
2024-10-20 11:11:01 +0200

    feat: wip

---
+++ test/ui/atoms/text_test.exs
@@ -0,0 +1,28 @@
+defmodule UI.Atoms.TextTest do
+  use ExUnit.Case, async: true
+  import Phoenix.Component
+  import Phoenix.LiveViewTest
+  alias UI.Atoms.Text
+
+  describe "Text component" do
+    test "renders h1" do
+      assigns = %{}
+      assert(
+        rendered_to_string(~H"""
+        <Text.h1>The text</Text.h1>
+        """) =~
+          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-h1 text-4xl user-select\">\n  The text\n</span>)
+      )
+    end
+
+    test "renders h2" do
+      assigns = %{}
+      assert(
+        rendered_to_string(~H"""
+        <Text.h2>The text</Text.h2>
+        """) =~
+          ~s(<span class=\"inline text-foreground break-normal whitespace-normal text-left tracking-normal font-sans font-normal leading-10 text-h2 user-select\">\n  The text\n</span>)
+      )
+    end
+  end
+end
